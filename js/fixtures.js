2c4ba222771329a556704c3f705f99d08d45c4b9
Bill Heaton <pixelhandler@gmail.com>
2014-01-12 23:26:52 -0800

    add a few content slides and title slide

--- js/fixtures.js
+++ js/fixtures.js
@@ -1,14 +1,22 @@
 /*global App*/
 App.Slide.FIXTURES = [
-  { id: '0', filename: 'http://fpoimg.com/800x600?text=Title'},
-  { id: '1', filename: 'http://fpoimg.com/800x600?text=Section-A', milliseconds: 1000 },
-  { id: '2', filename: 'http://fpoimg.com/800x600?text=Slide-A1', milliseconds: 500 },
-  { id: '3', filename: 'http://fpoimg.com/800x600?text=Slide-A2', milliseconds: 250 },
-  { id: '4', filename: 'http://fpoimg.com/800x600?text=Slide-A3' },
-  { id: '5', filename: 'http://fpoimg.com/800x600?text=Section-B', milliseconds: 300 },
-  { id: '6', filename: 'http://fpoimg.com/800x600?text=Slide-B1', milliseconds: 300 },
-  { id: '7', filename: 'http://fpoimg.com/800x600?text=Slide-B2', milliseconds: 300 },
-  { id: '8', filename: 'http://fpoimg.com/800x600?text=Slide-B3' },
-  { id: '9', filename: 'http://fpoimg.com/800x600?text=The End'},
-  { id: '10', iframeUrl: 'http://dev.w3.org/html5/markup/iframe.html#iframe'}
+  { id: 0, iframeUrl: './slides/intro/' },
+
+  { id: 1, iframeUrl: './slides/what-is-ember-data/' },
+
+  { id: 2, iframeUrl: './slides/emberjs/' },
+  { id: 3, iframeUrl: './slides/emberjs/consider.html' },
+  { id: 4, iframeUrl: './slides/emberjs/interactive.html' },
+
+  { id: 5, iframeUrl: './slides/chose-ember-data/' },
+  { id: 6, iframeUrl: './slides/chose-ember-data/fit.html' },
+  { id: 7, iframeUrl: './slides/chose-ember-data/beta.html' },
+
+  { id: 8, iframeUrl: './slides/core-concepts/' },
+  { id: 9, iframeUrl: './slides/core-concepts/store.html' },
+  { id: 10, iframeUrl: './slides/core-concepts/models.html' },
+  { id: 11, iframeUrl: './slides/core-concepts/records.html' },
+  { id: 12, iframeUrl: './slides/core-concepts/records-identified.html' },
+  { id: 13, iframeUrl: './slides/core-concepts/adapter.html' },
+  { id: 14, iframeUrl: './slides/core-concepts/adapter-example.html' }
 ];
